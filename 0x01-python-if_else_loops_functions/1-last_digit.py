02a0037c675b27f81484160dd106ec02de9d097c
Innocent <kaluinnocentokoro@gmail.com>
2021-11-17 05:21:26 -0800

    corrected PEP8 validation error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last_digit = -last_digit
 text1 = "Last digit of {} is {} and is greater than 5"
 text2 = "Last digit of {} is {} and is less than 6 and not 0"
-text3 = "last digit of {} is {} and is 0"
+text3 = "Last digit of {} is {} and is 0"
 if last_digit > 5:
     print(text1.format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
