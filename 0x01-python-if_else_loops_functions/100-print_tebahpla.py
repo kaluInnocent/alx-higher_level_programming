2deeb9764e9c43688d6acfe6e671ac5740cc14ff
Innocent <kaluinnocentokoro@gmail.com>
2021-11-16 16:32:18 -0800

    removed excess braces

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    print('{}'.format(chr(char - 32) if char % 2 != 0 else (chr(char))),\
-		    end='')
+    print('{}'.format(chr(char - 32) if char % 2 != 0 else chr(char)), end='')
