cc17dc08585062343f699ccbd6bc3886b36a7e6e
Innocent <kaluinnocentokoro@gmail.com>
2021-11-19 15:51:18 -0800

    added task 7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    idx = len(matrix)
+    for i in range(idx):
+        for j in range(len(matrix[i])):
+            print("{}".format(matrix[i][j]), end=" ")
+        print("$")
+
