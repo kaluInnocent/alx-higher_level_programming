7779ea6b319788eb253b494786e5b217e8fb8556
Innocent <kaluinnocentokoro@gmail.com>
2021-11-22 07:42:48 -0800

    corrected errors for pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     idx = len(matrix)
     for i in range(idx):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j < len(matrix[i]) - 1:
+                print("{}".format(" "), end="")
         print()
