e16d29fb4b53127f7b15e227a65e951feba3e023
Innocent <kaluinnocentokoro@gmail.com>
2021-12-06 22:03:24 -0800

    removed trailing white space

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,10 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
+        result = None
+    except FloatingPointError:
         result = None
     finally:
         print("Inside result: {}".format(result))
-	return result  
+    return result
