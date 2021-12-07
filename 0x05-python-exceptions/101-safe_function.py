d19a317878a3b9d9534885a83600b1d9f46daf4c
Innocent <kaluinnocentokoro@gmail.com>
2021-12-06 21:26:14 -0800

    completed tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)  
+    except Exception as err:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr) 
+        return None
