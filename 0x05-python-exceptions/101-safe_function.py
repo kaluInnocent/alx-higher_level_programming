0871cc2dc3b93c3881ae7c3da036f5d3d88917b1
Innocent <kaluinnocentokoro@gmail.com>
2021-12-06 21:33:26 -0800

    pip-8 error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_function(fct, *args):
+    import sys
     try:
-        return fct(*args)  
+        result = fct(*args)
+        return result  
     except Exception as err:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr) 
         return None
