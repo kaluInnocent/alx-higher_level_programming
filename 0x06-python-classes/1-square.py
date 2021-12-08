da7f2029c5bc459e31667adec598f35a90a89dcf
Innocent <kaluinnocentokoro@gmail.com>
2021-12-08 09:20:41 -0800

    added task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """Represents a class Square"""
+
+    def __init__(self, size):
+        """Initializes an instance of the square class
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
