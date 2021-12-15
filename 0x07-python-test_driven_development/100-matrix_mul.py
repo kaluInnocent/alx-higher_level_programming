a2726d2e6a928107c00541fe617428ff2a9545c4
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2021-12-15 15:22:10 +0000

    updated tasks

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,10 +8,8 @@ def matrix_mul(m_a, m_b):
     Args:
         m_a (list): first matrix
         m_b (list): second matrix
-
     Returns:
         result (list): A new matrix
-
     """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
@@ -21,7 +19,6 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]] or len(m_b) == 0:
         raise ValueError("m_b can't be empty")
-
     for row in m_a:
         if len(row) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
@@ -30,14 +27,12 @@ def matrix_mul(m_a, m_b):
         for n in row:
             if not isinstance(n, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
-
     for row in m_b:
         for n in row:
             if not isinstance(n, (int, float)):
                 raise TypeError("m_b should contain only integers or floats")
         if len(row) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
-
     result = []
     mul = 0
     for i in range(len(m_a)):
