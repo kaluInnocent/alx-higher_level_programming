316d4e6d3299043601b4dbd11265756ff9feff7d
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2021-12-21 22:50:30 +0000

    completed task 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function writes to a file"""
+
+
+def write_file(filename="", text=""):
+        """
+        Function writes a string to a text file and returns
+        the number of characters written
+        Args:
+                filename: file to be written into
+                text: file to be written
+
+        Returns: (int) The number of characters written
+
+        """
+        with open(filename, mode="w", encoding="utf-8") as f:
+           return f.write(text)
