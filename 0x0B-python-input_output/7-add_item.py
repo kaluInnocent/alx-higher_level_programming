959701371fe30ebc0fabff4f718018d0d3f69606
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-12-22 15:05:26 +0100

    updated task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Script ads alla arguments to a python list
-and saves them in a file 'added_item.json'
-"""
+""" Adds all the arguments to a python list and saves them in a file"""
 import sys
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
