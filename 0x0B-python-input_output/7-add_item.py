22d75b066747ed35d34f3cb005636a9d8acbe35c
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-12-22 10:14:25 +0100

    updated task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Script ads alla arguments to a python list
-   and saves them in a file 'added_item.json'
+and saves them in a file 'added_item.json'
 """
-
-
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-filename = "add_item.json"
 
-try:
-    content = load_from_json_file(filename)
-except FileNotFoundError:
-    content = []
-save_to_json_file(content + sys.argv[1:], filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
+    try:
+        content = load_from_json_file(filename)
+    except FileNotFoundError:
+        content = []
+    content.extend(sys.argv[1:])
+    save_to_json_file(content, filename)
