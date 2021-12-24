659a50411f3e03e45484199796337938d8ee743e
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-12-24 20:09:17 +0100

    completed task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry class"""
+
+
+class BaseGeometry:
+    """A class representing base geometry"""
+
+    def area(self):
+        """Method raises an exception"""
+        raise Exception("area() is not implemented")
