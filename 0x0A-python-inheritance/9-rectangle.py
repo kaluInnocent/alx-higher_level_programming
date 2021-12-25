0fd82f27ef167765f10571fb6aa76babd6701aa4
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-12-25 03:02:47 +0100

    completed task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Geometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """Method initializes an instance of a Rectangle object
+        Args:
+            width (int): Width of the Rectangle object
+            height (int): Height of the Rectangle object
+
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the area of Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Method returns a rectangle description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
