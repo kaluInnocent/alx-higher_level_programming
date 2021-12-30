bd0361a083df81261e869814271f5313e00f3bf9
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-12-30 20:35:22 +0100

    updated code for task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -143,3 +143,7 @@ class Rectangle(Base):
                     self.__x = value
                 elif key == "y":
                     self.__y = value
+    def to_dictionary(self):
+        """Method returns the dictionary representation of a Rectangle"""
+        return {'x': {self.__x}, 'y': {self.__y}, 'id': {self.id},
+                'height': {self.__height}, 'width': {self.__width}}
