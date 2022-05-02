6d55b1902fdc6aa06e36f6101454bdd4ec5df6f2
kaluInnocent <83899385+kaluInnocent@users.noreply.github.com>
2022-05-02 04:20:56 +0100

    Updated task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const process = require('process');
 // print process.argv
 if (process.argv.length - 2 > 1) {
   console.log('Arguments found');
-} else if (process.argv.length - 2 == 1) {
+} else if (process.argv.length - 2 === 1) {
   console.log('Argument found');
 } else {
   console.log('No argument');
