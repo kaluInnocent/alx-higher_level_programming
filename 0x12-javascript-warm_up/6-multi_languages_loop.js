c3dca6b3a6432da2e1533ea935057f5df2a9b8cf
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 15:31:36 +0100

    add task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+!#/usr/bin/node
+
+/*
+ * Script prints 3 lines
+ */
+
+const text = 'C is fun\nPython is cool\nJavaScript is amazing'
+
+for (let wd = 0; wd < text.length; wd++) {
+  console.log(`${wd}`);
+}
