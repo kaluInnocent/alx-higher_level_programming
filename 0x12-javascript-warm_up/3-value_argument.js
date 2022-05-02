2cbb14f766c58622e7bbf44872228eec1135fcdc
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 13:06:01 +0100

    update task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 
 /**
- * A script that prints the first argument
- * If no arguments are passed to are passed to tge script, print 'No argument'
- * You must use console.log(...) to print all output
- * You are not allowed to use var
- * You are not allowed to use length
+ * A script that prints argument passed to it
 */
 
-const arg = process.argv.slice(2);
-if (arg){
-	for (args in arg){
-		console.log(args);
-	}
-} else{
-	console.log('No argument');
+const myarg = process.argv.slice(2)
+
+if (myarg) {
+	console.log(myarg);
+} else {
+	cobsole.log('No argument');
 }
