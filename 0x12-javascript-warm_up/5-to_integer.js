079634690d4a77df0cf262f9b98f775ad95d4a6c
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 15:02:59 +0100

    add task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// prints number if the argument can be converted to an integer
+
+const args = process.argv;
+
+if (len(args) === 2) {
+  console.log('Not a number');
+} else {
+  if (isNaN(parseInt(args[2]))) {
+  console.log('Not a number');
+  } else {
+  console.log(`My number: ${parseInt(args[2]}`);
+  }
+}
