9d6b72dc103112511446290e1c5115b6b7bce9ca
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 15:14:59 +0100

    updated task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,7 +7,7 @@ const args = process.argv;
 if (args.length === 2) {
   console.log('Not a number');
 } else {
-    if (isNaN(parseInt(args[2]))) {
+  if (isNaN(parseInt(args[2]))) {
     console.log('Not a number');
   } else {
     console.log(`My number: ${parseInt(args[2])}`);
