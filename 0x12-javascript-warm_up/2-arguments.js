990b64256cbfb281ebbe58c3c5b6d4648a9d756f
InnocentKalu <obinnasmartkalu@gmail.com>
2022-05-02 01:50:24 +0000

    added task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+
+// print process.argv
+if (process.argv.length - 2 > 1) {
+  console.log('Arguments found');
+} else if (process.argv.length - 2 == 1) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
