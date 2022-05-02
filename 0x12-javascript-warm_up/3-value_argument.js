f41e42fd2f43d16084822e8fb17077f7569b5454
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 10:09:00 +0100

    updated taak

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 Const process = require('process');
-Const arg = process.argv.slice(2);
-if (arg[0] === null){
+if (process.argv[2] === null){
 console.log('No argument');
 } else{
-console.log(arg[0]);
+console.log(process.argv[2]);
 }
