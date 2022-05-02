a5a30b8d6e90b1f3099c178940873aa4e8339406
InnocentKalu <obinnasmartkalu@gmail.com>
2022-05-02 22:23:28 +0000

    updated code

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 /* Write a function that increments and calls a function.
-*	The function must be visible from outside
-*	Prototype: function (number, theFunction)
-*	You are not allowed to use var
+* The function must be visible from outside
+* Prototype: function (number, theFunction)
+* You are not allowed to use var
 */
 
 exports.addMeMaybe = function (num, func) {
