a35a71ceb659cdd0dc9f198ae6175fa8f8afb3fc
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-02 13:16:49 +0100

    update task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-/**
- * A script that prints argument passed to it
-*/
+// A script that prints argument passed to it
 
-const myarg = process.argv.slice(2)
+const myarg = process.argv.slice(2);
 
 if (myarg) {
-	console.log(myarg[0]);
+console.log(myarg[0]);
 } else {
-	cobsole.log('No argument');
+console.log('No argument');
 }
