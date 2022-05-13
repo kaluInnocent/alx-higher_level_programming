d22c3ed07423833c5b2d62bd6578445bf6a434ac
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-13 22:53:00 +0100

    added task 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- A script that creates a database called:
+-- hbtn_0c_0 in MYSQL server
+-- If the database hbtn_0c_0 already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
