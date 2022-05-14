aed8e4599ff6f2b6394e6309d61a6735c1a00746
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-14 14:10:05 +0100

    updated task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,21 @@
 -- Script converts a database to utf-8
 
 -- Convert database to utf-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- converts the table hbtn_0c_0
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+	hbtn_0c_0
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+-- converts the table hbtn_0c_0 to utf-8
+ALTER TABLE 
+	hbtn_0c_0.first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+-- convert the field 'name' to utf-8
+ALTER TABLE
+	hbtn_0c_0.first_table
+	MODIFY name
+	VARCHAR(256)
+	CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
