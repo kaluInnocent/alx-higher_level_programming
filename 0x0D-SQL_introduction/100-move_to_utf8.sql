7eafbe53d0c31f6b43d42ba9007b4e64f673c10c
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-14 14:13:05 +0100

    updated task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,8 +3,8 @@
 -- Convert database to utf-8
 ALTER DATABASE
 	hbtn_0c_0
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+	CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_unicode_ci;
 
 -- converts the table hbtn_0c_0 to utf-8
 ALTER TABLE 
