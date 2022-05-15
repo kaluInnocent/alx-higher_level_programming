cd232252b4e47768fbb5ef626f46c0236ff4616b
InnocentKalu <obinnasmartkalu@gmail.com>
2022-05-15 15:22:47 +0000

    updated task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- A script that lists all privileges of the MYSQL users 'user_0d_1' and 'user_0d_2' on your server
 
-SHOW GRANT FOR user_0d_1, user_0d_2;
+SHOW GRANT FOR user_0d_1@localhost, user_0d_2@localhost;
