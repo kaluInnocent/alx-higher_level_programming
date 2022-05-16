32a11c98d719fbdb79ca3019c1e6978df756f63e
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-16 14:48:57 +0100

    updated task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,7 +8,6 @@
 SELECT g.name AS genre, count(*) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS s
-ON g.id = s.show_id
-WHERE s.genre_id IS NOT NULL
+ON g.id = s.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
