fd7a8a58e6b159f79bdc55f42e0fb6e59064bde0
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-16 13:58:10 +0100

    added task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,8 +3,8 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
 
-SELECT s.id, g.genre_id
+SELECT s.title, g.genre_id
 FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
-ORDER BY s.id, g.genre_id ASC;
+ORDER BY s.title, g.genre_id ASC;
