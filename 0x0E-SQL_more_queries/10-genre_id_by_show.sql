4b19344d269e66ea2aff913143791f5f159d88d6
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-16 03:26:12 +0100

    updated task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tsh.title, tshg.id
-FROM tv_shows AS tsh INNER JOIN tv_show_genres AS tshg
-ON tsh.id = tshg.genre_id
-ORDER BY tsh.title, tshg.genre_id ASC;
+SELECT s.title, g.genre_id
+FROM tv_shows AS s INNER JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id ASC;
