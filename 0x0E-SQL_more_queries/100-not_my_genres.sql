995f1113f4b0ce4dcb9507d2ab1ffdc2a28a68d2
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-16 21:02:07 +0100

    updated task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,5 +9,13 @@ INNER JOIN tv_show_genres AS ts
 ON tg.id = ts.genre_id
 INNER JOIN tv_shows AS t
 ON t.id = ts.show_id
-WHERE t.title != "Dexter"
+WHERE name NOT IN (
+	SELECT name
+	FROM tv_genres AS tg
+	INNER JOIN tv_show_genres AS ts
+	ON tg.id = ts.genre_id
+	INNER JOIN tv_shows AS t
+	ON t.id = ts.show_id
+	WHERE t.title = "Dexter"
+)
 ORDER BY name ASC;
