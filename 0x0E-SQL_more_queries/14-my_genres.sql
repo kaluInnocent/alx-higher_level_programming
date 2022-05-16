6f0dcbbc555f1a54351370a918ab1031c63563ac
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-16 18:22:42 +0100

    updated task

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,7 +5,9 @@
 
 SELECT  tg.name AS name
 FROM tv_genres AS tg
-INNER JOIN tv_shows AS ts
-ON tg.id = ts.id
-WHERE ts.title = "Dexter"
+INNER JOIN tv_show_genres AS ts
+ON tg.id = ts.genre_id
+INNER JOIN tv_shows AS t
+ON t.id = ts.show_id
+WHERE t.title = "Dexter"
 ORDER BY name ASC;
