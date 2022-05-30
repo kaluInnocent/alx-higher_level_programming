6aff26ff385cc930188ba956e895cdd10f34fae5
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-05-30 01:58:21 +0100

    updated task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       super.print();
