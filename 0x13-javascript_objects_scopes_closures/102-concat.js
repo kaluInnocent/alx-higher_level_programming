7e94f003fac64a236ea8960a547a59e107ef3bf4
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-05-30 19:22:58 +0100

    add task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 
-const myArg = process.argv.slice(2);
-console.log(myArg[0].concat(myArg[1], myArg[2]));
+const file = require('file');
+const arg1 = file.readFileSync(process.argv[2], 'utf8');
+const arg2 = file.readFileSync(process.argv[3], 'utf8');
+file.writeFileSync(process.argv[4], arg1 + arg2);
