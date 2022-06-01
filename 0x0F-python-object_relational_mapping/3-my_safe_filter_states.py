cda6dc51e0c52548542f5cbf785f01678a03ecd1
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-01 21:01:57 +0100

    message

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script lists all states starting with N in a database
+Usage: ./0-select_states.py username password database-name"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+            )
+    cur = db.cursor()
+    cur.execute(
+            """SELECT *
+            FROM states
+            WHERE name = %s
+            ORDER BY id ASC
+            """,
+           ( sys.argv[4],)
+            )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
