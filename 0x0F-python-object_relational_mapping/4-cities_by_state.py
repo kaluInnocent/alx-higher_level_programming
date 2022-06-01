0076c9442d1bdf68baa945fb2fee65f2350d15d9
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-01 21:15:07 +0100

    message

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script lists all cities in a database
+Usage: ./0-select_states.py username password database-name"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+            )
+    cur = db.cursor()
+    cur.execute(
+            """SELECT *
+            FROM cities
+            ORDER BY id ASC
+            """
+            )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
