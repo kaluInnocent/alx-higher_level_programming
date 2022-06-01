68919f0c6d2eea6a86d6c1368a32ea72d67f083e
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-01 22:03:46 +0100

    message

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             FROM cities
             INNER JOIN states
             ON cities.state_id = states.id
-            WHERE state.name = %s
+            WHERE states.name = %s
             ORDER BY cities.id ASC;
             """,
             (sys.argv[4],)
