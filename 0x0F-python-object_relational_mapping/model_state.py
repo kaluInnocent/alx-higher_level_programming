7f19dcc5e0241360c3e4cd3d500238ca29033b27
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-02 15:21:30 +0100

    message

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,9 +4,7 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
-
 Base = declarative_base()
-"""States define a state class"""
 
 
 class State(Base):
