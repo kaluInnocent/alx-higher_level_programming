4d5d47b226274fe5824134daa19e4b7cdb27aedd
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-02 22:38:28 +0100

    updated task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script lists all State objects from the database hbtn_0e_6_usa"""
+"""This Script lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
