21bbcc9fc258dcc5d4ecb175cb7788b6181192ea
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-02 22:10:20 +0100

    message

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import States, Base
+
+#creating a connection
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    #extract a session
+    session = Session()
+    #querry a session
+    states = (
+            session
+            .querry(State)
+            .group_by(State.id)
+            .all())
+    for state in states:
+        print(f"{state.id}: {state.name}")
+    session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-"""Has class definition of a State and an instance Base = declarative_base()"""
+""" python file that contains the class definition of a State and an instance Base = declarative_base()
+"""
 
-
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
 
 class State(Base):
-    """class that inherits from Base"""
+    """A class states that inherits from sqlalchemy declarative_base()
+
+    Attributes
+    -----------
+    id: state id (int)
+    name: state name (str)
+    """
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    id = Column(
+            Interger,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            autoincrement=True
+            )
+    name = Column(
+            String(128),
+            nullabe=False
+            )
