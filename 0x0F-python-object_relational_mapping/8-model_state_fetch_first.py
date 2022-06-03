acc9ae9000898c4f2503578a8578571173d589d8
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-03 14:45:54 +0100

    message

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Script that  lists all State objects from the database hbtn_0e_6_usa
+"""A script lists all State objects from the database hbtn_0e_6_usa"""
 
-Usage:  ./7-model_state_fetch_all.py <mysql username> <mysql password> <database name>
-"""
 import sys
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
-
-from sqlalchemy.orm import sessionmaker
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
+    try:
+        print("{:}: ".format(session.query(State).order_by(
+            State.id).first().id), end="")
+        print(session.query(State).order_by(State.id).first().name)
+    except:
         print("Nothing")
+    session.close()
