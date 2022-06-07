27af00e03e7ba234a57767d251ac5fc97e82b3fc
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-07 16:05:02 +0100

    message

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """List State in database
 """
+
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
