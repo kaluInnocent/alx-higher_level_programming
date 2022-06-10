38f7dd49c370b9760daca97020ad8cf7014e1518
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-10 15:22:25 +0100

    message

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script that prints all City objects from the database hbtn_0e_14_usa"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+""" Script that prints all City objects from the database hbtn_0e_14_usa""" 
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import  create_engine
+    from sqlalchemy.orm import sessionmaker
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
         sys.argv[2],
