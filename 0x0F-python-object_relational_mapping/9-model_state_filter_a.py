4058fc0b11981e21caa0925e688e731856b20777
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-10 14:57:15 +0100

    message

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,14 +5,18 @@
 if __name__ == "__main__":
     import sys
     from model_state import State, Base
-    from sqlalchemy import(create_engine)
+    from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).filter(State.name.contains('a%'))
+    states = session.query(State).order_by(State.id)
     for state in states:
-        print("{}: {}".format(state.id, state.name))
-    
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
