3e26dfdea94ccbc62871e80e8910d4ce96a4d18f
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-10 14:35:47 +0100

    message

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
     session.close()
