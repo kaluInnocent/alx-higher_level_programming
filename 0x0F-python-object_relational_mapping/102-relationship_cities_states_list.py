8d2b0121bcd82c0c38a14a5c1b26d0425e704b76
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-10 14:10:31 +0100

    updated task

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Write a script that lists all City objects
-#from the database hbtn_0e_101_usa
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
