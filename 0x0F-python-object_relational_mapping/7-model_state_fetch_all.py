265d05ca916f1978d31657a5bcbd49aec262a247
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-10 14:29:31 +0100

    message

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module for Base, State, and SQLALCHEMY"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
         sys.argv[2],
