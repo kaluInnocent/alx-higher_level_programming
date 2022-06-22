d958a7134b1de59067a90757916301a6cb629ae2
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-22 20:09:56 +0100

    message

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={"q": q})
+    try:
+        response = response.json()
+        if response == {} or len(response) == 0:
+            print("no result")
+        else:
+            print(
+                    f"[{response.get('id')}] {response.get('name')}")
+    except ValueError:
+        print("Not a valid json")
