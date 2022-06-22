e2b3f9efa595776179638edc25dc76802407ba76
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-06-22 19:26:13 +0100

    message

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code")
+        print(f"Error code: {response.status_code}")
     else:
         print("{}".format(response.text))
